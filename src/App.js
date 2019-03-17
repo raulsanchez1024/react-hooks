85242dc0fbaea4625135bd8d734a98f2ba1d9e66
Raul <raulsanchez1024@gmail.com>
2019-03-17 15:45:31 -0600

    Converted class componentsto functional componentt, also used useState

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('Sofi');
+
+  function handleNameChange(e) {
+    setName(e.target.value);
   }
+
+  return (
+    <div>
+      <p>You liked { count } times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+
+      <p>Hi my name is { name } 👧🏼!</p>
+      <input type="text" name="name" onChange={handleNameChange} />
+    </div>
+  );
 }
 
 export default App;
