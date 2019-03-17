3cd3acd7435966ed0574ed5057634ca50b61f8ea
Raul <raulsanchez1024@gmail.com>
2019-03-17 15:53:28 -0600

    useEffect()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState('Sofi');
+  const [title, setTitle] = useState('react-hooks');
+
+  // this works like ComponentDidMount()
+  useEffect(() => {
+    document.title = title;
+  });
 
   function handleNameChange(e) {
     setName(e.target.value);
+  }
+
+  function handleTitleChange(e) {
+    setTitle(e.target.value);
   }
 
   return (
@@ -18,8 +28,12 @@ function App() {
 
       <p>Hi my name is { name } 👧🏼!</p>
       <input type="text" name="name" onChange={handleNameChange} />
+
+      <p>Change the title of this page 👇</p>
+      <input type="text" name="title" onChange={handleTitleChange} />
     </div>
   );
 }
 
 export default App;
+
